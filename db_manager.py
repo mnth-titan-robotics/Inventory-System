5a11c95b44eb13e793cb6c0fea65a6da73a3e631
RightHandedVoid <righthandedvoid@gmail.com>
2025-12-11 21:30:45 -0600

    Added Pseudo-Code to "Database Manager" to dictate the initalization of SQL Tables

--- db_manager.py
+++ db_manager.py
@@ -1,20 +1,6 @@
 import sqlite3
 
 
-# Does db file exist?
-# Yes:
-#   Does schema need to be updated?
-#       Yes:
-#           make new db file of new schema
-#           migrate old db to new schema
-#           replace old db
-#       No:
-#           Do nothing
-# No:
-#   create new db file of latest schema
-#   go back to start
-
-
 class DatabaseManager:
     def __init__(self):
         self.db_file = "TEXPLOSION.db"
@@ -30,13 +16,45 @@ class DatabaseManager:
         # "Startup" functions
         self._setup_db()
         self._define_db()
+
+    def _FETCH(self, command: str) -> list | Exception:
+        try:
+            self.db_cursor.execute(command)
+            return self.db_cursor.fetchall()
+        
+        except Exception as e:
+            return Exception(e)
+
+    def _EXECUTE(self, command: str) -> None:
+        self.db_cursor.execute(command)
     
     def _setup_db(self):
         self.db_connection = sqlite3.connect(self.db_file)
         self.db_cursor = self.db_connection.cursor()
 
     def _define_db(self):
-        for TABLE_NAME in self.db_schema:
-            CMD = f"{TABLE_NAME}({",".join(self.db_schema[TABLE_NAME])})".replace(" ", "_")
+        CMD_QUEUE = []
 
-            self.db_cursor.execute(f"CREATE TABLE {CMD}")
+        # Create SQL Commands from db_schema variable
+        for TABLE_NAME in self.db_schema:
+            STRUCTURE = {
+                "SQL_COMMAND": (f"{TABLE_NAME}({",".join(self.db_schema[TABLE_NAME])})".replace(" ", "_")),
+                "TABLE_NAME": (TABLE_NAME),
+                "TABLE_COLUMNS": (self.db_schema[TABLE_NAME])
+            }
+
+            CMD_QUEUE.append(STRUCTURE)
+
+        # Execute SQL Commands from SQL_QUEUE
+
+        # Main Cases
+        # If table exists and needs to be deleted
+        # If table does not exist and need to exist
+        # If table exists and does exist
+        # If table does not exist and needs to be deleted
+
+        # If table needs to add rows which do not exist
+        # If table needs to remove rows which do exist
+        # If table needs to have rows which do not exist
+        # If table needs to add rows which do exist
+        # If table needs to remove rows which do not exist
